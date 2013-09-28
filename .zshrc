d18b339c2dcde20d386afe059f169ae8c7076517
Enming Wu <enmingdavidwu@gmail.com>
2013-09-28 18:10:13 -0400

    Moved local git repo. to home_dir directory.

---
+++ .zshrc
@@ -0,0 +1,7 @@
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+alias vimrc='vim ~/.vimrc'
+alias downloads='cd ~/Downloads'
+alias excel='open -a /Applications/Microsoft\ Office\ 2011/Microsoft\ Excel.app'
+alias word='open -a /Applications/Microsoft\ Office\ 2011/Microsoft\ Word.app'
+
+alias zshrc='vim ~/.zshrc'
--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# Installs Vundle
+rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global
+
+# Create hard links in $HOME directory
+ln .vimrc ~/.vimrc
+ln .zshrc ~/.zshrc
+ln .gitignore_global ~/.gitignore_global
+
+# Installs Vundle in $HOME directory
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 # Installs or updates all bundles in vundle 
 vim +BundleInstall! +qall
 
+# Reload .zshrc
+source .zshrc
