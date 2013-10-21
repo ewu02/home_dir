5d19bc637a9a3bc43a4c2c37875ad96142e4675d
Enming Wu <enmingdavidwu@gmail.com>
2013-10-21 00:02:02 -0400

    Added oh-my-zsh.

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
+# Installs oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# Removes configuration files in $HOME directory
 rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global
 
-# Create hard links in $HOME directory
+# Create hard links in $HOME directory to configuration files
 ln .vimrc ~/.vimrc
 ln .zshrc ~/.zshrc
 ln .gitignore_global ~/.gitignore_global
@@ -12,6 +16,3 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 # Installs or updates all bundles in vundle 
 vim +BundleInstall! +qall
-
-# Reload .zshrc
-source .zshrc
