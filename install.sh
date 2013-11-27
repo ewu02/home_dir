e8604e9635ba153f8d5128e74dfa996e4f746747
Enming Wu <enmingdavidwu@gmail.com>
2013-11-27 16:27:15 -0500

    Switched to symbolic links.

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 # Removes configuration files in $HOME directory
 rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global
 
-# Create hard links in $HOME directory to configuration files
-ln .vimrc ~/.vimrc
-ln .zshrc ~/.zshrc
-ln .gitignore_global ~/.gitignore_global
+# Create symbolic links in $HOME directory to configuration files
+ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s $PWD/.zshrc $HOME/.zshrc
+ln -s $PWD/.gitignore_global $HOME/.gitignore_global
 
 # Installs Vundle in $HOME directory
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
--- update_conf_links.sh
+++ update_conf_links.sh
@@ -3,7 +3,8 @@
 # Removes configuration files in $HOME directory
 rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global
 
-# Create hard links in $HOME directory to configuration files
-ln .vimrc ~/.vimrc
-ln .zshrc ~/.zshrc
-ln .gitignore_global ~/.gitignore_global
+# Create symbolic links in $HOME directory to configuration files
+ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s $PWD/.zshrc $HOME/.zshrc
+ln -s $PWD/.gitignore_global $HOME/.gitignore_global
+
