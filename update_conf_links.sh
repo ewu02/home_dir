f44fd3a9227b6985d1def6cf59f8052aa297bf66
Enming Wu <enmingdavidwu@gmail.com>
2014-07-03 15:30:36 -0400

    cleanup

--- update_conf_links.sh
+++ update_conf_links.sh
@@ -1,11 +1,10 @@
 # #!/bin/bash
 
 # Removes configuration files in $HOME directory
-rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global ~/.NERDTreeBookmarks
+rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global
 
 # Create symbolic links in $HOME directory to configuration files
 ln -s $PWD/.vimrc $HOME/.vimrc
 ln -s $PWD/.zshrc $HOME/.zshrc
 ln -s $PWD/.gitignore_global $HOME/.gitignore_global
-ln -s $PWD/.NERDTreeBookmarks $HOME/.NERDTreeBookmarks
 
