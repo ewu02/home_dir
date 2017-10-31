da6729753c57858beaac5e9b882956daee3aa9a7
Enming Wu <enmingdavidwu@gmai.com>
2017-10-30 17:51:55 -0700

    removed nerdtreebookmark ref from install script

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 # Removes configuration files in $HOME directory
-rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global ~/.NERDTreeBookmarks ~/.editorconfig
+rm -f ~/.vimrc ~/.zshrc ~/.gitignore_global ~/.editorconfig
 
 # Create symbolic links in $HOME directory to configuration files
 ln -s $PWD/.vimrc $HOME/.vimrc
 ln -s $PWD/.zshrc $HOME/.zshrc
 ln -s $PWD/.gitignore_global $HOME/.gitignore_global
-ln -s $PWD/.NERDTreeBookmarks $HOME/.NERDTreeBookmarks
 ln -s $PWD/.editorconfig $HOME/.editorconfig
 
 # Installs homebrew
